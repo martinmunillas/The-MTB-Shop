701ca70f5c83b61e9c65c60e0b29b0f7708b67fa
Martin <martinmunillas@gmail.com>
2020-07-23 10:50:08 -0400

    added more or less amount buttons in the cart and also added a remove button

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,28 +1,84 @@
-import React from 'react'
+import React from "react";
+import { connect } from 'react-redux'
 
-import '../assets/styles/components/CartItem.scss'
+import { addToCart, removeFromCart } from "../redux/actions";
+
+import "../assets/styles/components/CartItem.scss";
 import bike from "../assets/static/products/bike.svg";
 
 const CartItem = (props) => {
-    const {
-        id,
-        name,
-        description,
-        types,
-        imageSrc,
-        price,
-        discount,
-        amount,
-        exchange,
-      } = props;
+  const {
+    id,
+    name,
+    description,
+    types,
+    imageSrc,
+    price,
+    discount,
+    exchange,
+  } = props;
 
-    return (
-        <div className="cartItem">
-            <img src={bike} alt="" className="cartItem_image"/>
-            <h2 className="cartItem_name">{name}</h2>
-            <p className="cartItem_amount">Amount {amount}</p>
-        </div>
-    )
-}
+  let {
+    amount,
+  } = props
 
-export default CartItem
+  const handleRemove = () => {
+    props.removeFromCart({ id })
+  }
+
+  const handleLessAmount = () => {
+    --amount;
+    if(amount < 1) {
+        props.removeFromCart({ id })
+    } else {
+        props.addToCart({
+          id,
+          name,
+          description,
+          types,
+          imageSrc,
+          price,
+          discount,
+          amount,
+        });
+    }
+  };
+
+  const handleMoreAmount = () => {
+    amount++;
+    props.addToCart({
+      id,
+      name,
+      description,
+      types,
+      imageSrc,
+      price,
+      discount,
+      amount,
+    });
+  };
+
+  return (
+    <div className="cartItem">
+      <img src={bike} alt="" className="cartItem_image" />
+      <h2 className="cartItem_name">{name}</h2>
+      <p className="cartItem_amount">Amount {amount}</p>
+      <button className="moreAmount" onClick={handleMoreAmount}>
+        more
+      </button>
+      <button className="lessAmount" onClick={handleLessAmount}>
+        less
+      </button>
+      <button className="remove" onClick={handleRemove}>remove</button>
+    </div>
+  );
+};
+
+
+
+const mapDispatchToProps = {
+    addToCart,
+    removeFromCart,
+  }  
+
+export default connect(null, mapDispatchToProps)(CartItem);
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,11 +1,24 @@
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
-      const exist = state.cart.find(item => item.id === action.payload.id)
+    case "ADD_TO_CART":
+      const exist = state.cart.find((item) => item.id === action.payload.id);
       return {
         ...state,
-        cart: exist ? [...state.cart.filter(item => item.id !== action.payload.id), action.payload] : [...state.cart, action.payload]
-      }
+        cart: exist
+          ? [
+              ...state.cart.filter((item) => item.id !== action.payload.id),
+              action.payload,
+            ]
+          : [...state.cart, action.payload],
+      };
+
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: [
+          ...state.cart.filter((item) => item.id !== action.payload.id)
+        ],
+      };
     default:
       return state;
   }
